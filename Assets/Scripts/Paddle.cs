24626267be776655a6affc326bf87b3bd11ba463
TristenKGM <tristenkreutz@gmail.com>
2021-01-20 09:57:11 -0700

    Movement of paddle now tied to mouse

---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    // Configuration Parameters
+    [Tooltip("The total number of world units as a numeric value.")]
+    [SerializeField] float worldUnits = 16f;
+    [SerializeField] float paddleYPos = 0.5f;
+    [SerializeField] float minX = 1f, maxX = 15f;
+
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        MovePaddle();
+    }
+
+    private void MovePaddle()
+    {
+        float mousePosInUnits = Input.mousePosition.x / Screen.width * worldUnits;
+        float clampedMousPosInUnits = Mathf.Clamp(mousePosInUnits, minX, maxX);
+        Vector2 paddlePos = new Vector2(clampedMousPosInUnits, paddleYPos);
+        this.transform.position = paddlePos;
+    }
+}
