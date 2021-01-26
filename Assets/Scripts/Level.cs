bd8defe12332e7083305abf468b05df2482c167a
TristenKGM <tristenkreutz@gmail.com>
2021-01-26 01:09:18 -0700

    Sound + Block Count + Level Load
    Sounds added to blocks. Blocks are now counted and next level loads when count reaches 0.

---
+++ Assets/Scripts/Level.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level : MonoBehaviour
+{
+    int numBlocks;
+    [SerializeField] Loader loader;
+
+    public void AddBlock()
+    {
+        numBlocks++;
+        Debug.Log(numBlocks);
+    }
+
+    public void RemoveBlock()
+    {
+        numBlocks--;
+        Debug.Log(numBlocks);
+        if (numBlocks <= 0)
+        {
+            loader.LoadNextLevel();
+        }
+    }
+}
