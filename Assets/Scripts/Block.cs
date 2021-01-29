926ae719f743891664d3d43d1868969c703a9fe1
TristenKGM <tristenkreutz@gmail.com>
2021-01-29 15:20:53 -0700

    Added particle VFX, unbreakable blocks, and started documentation
    Added particle VFX to play when block is destroyed. Some blocks can now be unbreakable. Starting to go back through code and document methods.

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -6,6 +6,7 @@ public class Block : MonoBehaviour
 {
     // Parameters
     [SerializeField] AudioClip breakSound;
+    [SerializeField] GameObject blockBreakParticleVFX;
 
     // Cached References
     Level level;
@@ -13,22 +14,61 @@ public class Block : MonoBehaviour
 
     private void Start()
     {
-        level = FindObjectOfType<Level>();
-        gameState = FindObjectOfType<GameState>();
-
-        level.AddBlock();
+        AddBlockToLevel();
     }
 
+    /* AddBlockToLevel checks to see if the block is breakable. 
+     * If true, it caches references to the Level and Game State game objects. 
+     * It then calls a public method in Level to add itself to the block count. 
+     */
+    private void AddBlockToLevel()
+    {
+        if (CompareTag("Breakable"))
+        {
+            level = FindObjectOfType<Level>();
+            gameState = FindObjectOfType<GameState>();
+
+            level.AddBlock();
+        }
+    }
+
+    /* Defines the OnCollisionEnter2D event.
+     * If the block is breakable, it calls the DestroyBlock method.
+     */
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        DestroyBlock();
+        if (CompareTag("Breakable"))
+        {
+            DestroyBlock();
+        }
     }
 
+    /* DestroyBlock calls a series of methods to perform operations related to block destruction.
+     * This includes: triggering SFX, removing block from count, adding to game score, triggering
+     * particle VFX, and finally destroying itself.
+     */
     private void DestroyBlock()
     {
-        AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
+        TriggerBreakSFX();
         level.RemoveBlock();
         gameState.AddToScore();
+        TriggerBreakParticleVFX();
         Destroy(gameObject);
+    }
+
+    /* TriggerBreakSFX creates an audio source at the position of the game camera and plays the associated clip.
+     */
+    private void TriggerBreakSFX()
+    {
+        AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
+    }
+
+    /* TriggerBreakParticleVFX instantiates a new game object at this object's position to play a VFX.
+     * After 0.5 seconds, the VFX game object is then destroyed.
+     */
+    private void TriggerBreakParticleVFX()
+    {
+        GameObject blockBreakParticles = Instantiate(blockBreakParticleVFX, transform.position, transform.rotation);
+        Destroy(blockBreakParticles, 0.5f);
     }
 }
